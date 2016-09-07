968fca88ca0573c591375500f106238ae1a12fff
Christopher Cote <chris@entropealabs.com>
2016-09-07 17:05:15 -0500

    adds test for event bus

--- test/meteo_stick_test.exs
+++ test/meteo_stick_test.exs
@@ -5,4 +5,9 @@ defmodule MeteoStickTest do
   test "the truth" do
     assert 1 + 1 == 2
   end
+
+  test "events" do
+      MeteoStick.EventManager.add_handler(MeteoStick.Handler)
+      assert_receive %MeteoStick.WeatherStation.State{}, 30_000
+  end
 end
