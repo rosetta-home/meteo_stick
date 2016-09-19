eb33587e44ad92d520873bd83e66e58838f45a47
Christopher Cote <chris@entropealabs.com>
2016-09-19 12:04:37 -0500

    auto-discover UART port

--- lib/meteo_stick.ex
+++ lib/meteo_stick.ex
@@ -1,8 +1,22 @@
 defmodule MeteoStick do
-    use Application
-    require Logger
+  use Application
+  require Logger
+  alias Nerves.UART, as: Serial
 
-    def start(_type, _args) do
-        {:ok, pid} = MeteoStick.Supervisor.start_link
-    end
+  def start(_type, _args) do
+    get_tty
+    {:ok, pid} = MeteoStick.Supervisor.start_link
+  end
+
+  def get_tty do
+    Serial.enumerate |> Enum.each(fn({tty, device}) ->
+      case device.product_id do
+        24577 ->
+          Logger.info("Setting Meteo TTY: #{inspect tty}")
+          Application.put_env(:meteo_stick, :tty, "/dev/#{tty}", persistent: true)
+        _ -> nil
+      end
+    end)
+  end
+
 end
